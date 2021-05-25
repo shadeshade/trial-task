0e1250ccc8338b2d45ed10451f2fbf6855a28bfb
shade916 <privetabdul@gmail.com>
2021-05-25 13:53:34 +0300

    refactor get_prime_nums

--- task1.py
+++ task1.py
@@ -1,8 +1,8 @@
 def get_prime_nums(n):
     prime_nums = []
     if n > 1:
-        for i in range(2, int(n / 2) + 1):
-            for j in range(2, i):
+        for i in range(2, n):
+            for j in range(2, int(i / 2) + 1):
                 if i % j == 0:
                     break
             else:
@@ -11,4 +11,4 @@ def get_prime_nums(n):
 
 
 if __name__ == '__main__':
-    print(get_prime_nums(199))
+    print(get_prime_nums(100))
